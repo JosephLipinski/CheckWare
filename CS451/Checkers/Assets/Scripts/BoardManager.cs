546d5e640446c44d601b69367feabe2afc3ff1c7
Joe Lipinski <jsl94@drexel.edu>
2018-02-19 18:16:05 -0500

    Initial commit

---
+++ CS451/Checkers/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+	ArrayList bluePositions = new ArrayList(new string[] {"A1","A3","A5","A7","B2","B4","B6","B8","C1","C3","C5","C7"});
+	ArrayList purplePositions = new ArrayList(new string[] {"F2","F4","F6","F8","G1","G3","G5","G7","H2","H4","H6","H8"});
+
+	public GameObject blueChecker, blueKing, purpleChecker, purpleKing;
+
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			if (bluePositions.Contains (child.name)) {
+				Vector3 spawnPosition = child.transform.position + new Vector3 (0f, 1f, 0f);
+				Instantiate (blueChecker, spawnPosition, Quaternion.identity);
+
+			}
+			else if(purplePositions.Contains(child.name)){
+				Vector3 spawnPosition = child.transform.position + new Vector3 (0f, 1f, 0f);
+				Instantiate (purpleChecker, spawnPosition, Quaternion.identity);			
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ CS451/Checkers/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+
+	public NetworkIdentity myId;
+	
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+	void Start()
+	{
+		if (isServer)
+			GetComponent<MeshRenderer>().material.color = Color.black;
+	}
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+		//if (isClient)
+		//	print ("I am the host");
+		
+		
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
