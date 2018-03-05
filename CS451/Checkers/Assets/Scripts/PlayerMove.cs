159a53ef9f0c35c5f94e6dc17088ad61d847b35f
Matthew Wagar <mrw77@drexel.edu>
2018-03-05 11:20:13 -0500

    Player controller now leps to each piece via mouse position
    -Replaced board with prefab board
    -Changed player controller to have raycast and lerp movment
    -Added board layer

--- CS451/Checkers/Assets/Scripts/PlayerMove.cs
+++ CS451/Checkers/Assets/Scripts/PlayerMove.cs
@@ -3,8 +3,12 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
+	Ray ray;
+	RaycastHit hit;
 
 	public NetworkIdentity myId;
+	public LayerMask mask = -1;
+	public float smooth = 5.0f;
 	
 	public override void OnStartLocalPlayer()
 	{
@@ -28,5 +32,19 @@ public class PlayerMove : NetworkBehaviour
 		var z = Input.GetAxis("Vertical")*0.1f;
 
 		transform.Translate(x, 0, z);
+
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         if(Physics.Raycast(ray, out hit, 100, mask.value))
+         {
+             print (hit.collider.name);
+
+             Vector3 boardLoc = new Vector3(hit.collider.gameObject.transform.position.x, 0.25f,hit.collider.gameObject.transform.position.z);
+
+             //transform.SetPositionAndRotation(boardLoc, transform.rotation);
+
+             transform.position = Vector3.Lerp (transform.position, boardLoc, Time.deltaTime * smooth);
+
+
+         }
 	}
 }
