9c4238680a21a0dec6ceb2e1549baab92e91db32
Joe Lipinski <jsl94@drexel.edu>
2018-03-04 23:32:31 -0500

    Trying some stuff

---
+++ CS451/Checkers/Assets/Scripts/GameState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameState : NetworkBehaviour {
+	public enum state {noPlayers, onePlayer, readyToGo, Done} ;
+	public static state myState;
+
+
+
+	// Use this for initialization
+	void Start () {
+		myState = state.noPlayers;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlayerConnected()
+	{
+		print (myState.ToString ());
+
+		if (myState == state.noPlayers) {
+			myState = state.onePlayer;
+		}
+		else{
+			myState = state.readyToGo;
+		}
+
+		print (myState.ToString ());
+	}
+}
